2aafe57a31555f10023995356336d2e625028743
Gianna Sanchez <giannaaaaa1@gmail.com>
2019-10-24 10:59:13 -0400

    midterm

--- classexamples/seasons/sketch.js
+++ classexamples/seasons/sketch.js
@@ -4,6 +4,19 @@ var seasonType = "";
 var sunX = 0;
 var sunY = 0;
 
+var snowX = [];
+var snowY = [];
+var snowAmount = 100;
+
+var leafX = [];
+var leafY = [];
+var leafAmount = 100;
+var leafImage;
+
+function preload(){
+  leafImage = loadImage("leaf.png");
+}
+
 function setup() {
   // put setup code here
 
@@ -20,6 +33,23 @@ function setup() {
   });
   sunX = width/2;
   sunY = height * 2;
+
+  //var i=0 is our start point
+  //i<100 is our conditional, how many times do we want the for loop to run
+  //i++ which is the same as saying i=i+1
+  for(var i = 0; i<snowAmount; i++){
+    snowX[i] = random(0,width); //random number is from 0-width-1 or 0-399
+    snowY[i] = random(0, -500);
+  }
+  console.log("snowX " + snowX);
+  console.log("snowY " + snowY);
+  //var i is destoryed after for loop is done
+  for(var i = 0; i < leafAmount; i++){
+    leafX[i] = random(0,width);
+    leafY[i] = random(0,-500)
+  }
+  console.log("leafX " + leafX);
+  console.log("leafY " + leafY);
 }
 
 
@@ -28,22 +58,43 @@ function draw() {
   background(255);
   if(seasonType == "fall"){
     console.log("fall");
+
+    for(var f = 0; f<leafAmount; f++){
+      if(leafY[f]<height-20){
+        leafY[f]++;
+        leafX[f] = leafX[f] + sin(radians(frameCount));
+        //console.log("leafY " + f + " " + leafY[f]);
+      }
+
+      image(leafImage, leafX[f], leafY[f],20,20);
+    }
+
   }else if(seasonType == "winter"){
     console.log("winter");
+    fill(255);
+    stroke(0);
+
+    for(var y = 0; y<snowAmount; y++){
+      snowY[y]++; //same as nowY[y] = snowY[y]+1
+      snowY[y] = snowY[y] +5;
+      ellipse(snowX[y],snowY[y],10,10);
+      if(snowY[y] > height){
+        snowY[y] = random(0,-500);
+        snowX[y] = random(0,width);
+      }
+    }
+
   }else if(seasonType == "spring"){
     console.log("spring");
   }else if(seasonType == "summer"){
     console.log("summer");
     stroke("red");
-    fill("yellow");
+    fill("orange");
     ellipse(sunX,sunY,width,height);
 
     if(sunY > height){
       sunY = sunY -1;
     }
 
-  }else{
-    console.log("blank");
-    text("Make a selection" 100,20);
   }
 } //end of draw
